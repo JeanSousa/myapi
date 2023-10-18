b6f7c81dff50b5848b76c4496a515d165a2e5277
jeanjr123 <jeanjr.silvasousa@gmail.com>
2023-10-18 08:20:10 -0300

    install express and create get route

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,8 +1,18 @@
-export class AppServer {
-  // propriedade app tipada como string
-  private app: string
+import express from 'express'
+import 'express-async-errors'
 
-  constructor(info: string) {
-    this.app = info ?? 'Ola dev'
-  }
-}
+//criando instancia da aplicação
+const app = express()
+
+// informando para o express validar informações com formato json
+app.use(express.json())
+
+app.get('/', (request, response) => {
+  return response.json({
+    message: 'Ola Dev!',
+  })
+})
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000!')
+})
