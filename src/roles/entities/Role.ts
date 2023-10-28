8ce598a6108d29aef662a27e32155607684a93b5
jeanjr123 <jeanjr.silvasousa@gmail.com>
2023-10-27 23:16:58 -0300

    Refactor role entity

--- src/roles/entities/Role.ts
+++ src/roles/entities/Role.ts
@@ -1,9 +1,21 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm'
 import { v4 as uuidv4 } from 'uuid'
 
+// fazendo o mapeamento da entidade com decorators para a tabela do banco de dados
+// o decorator @Entity('nome tabela mapeada') serve para mostrar que a classe é uma entidade para o type orm
+// obs: essa entidade precisa ser importada no arquivo index.ts na instancia do datasource
+@Entity('roles')
 export class Role {
   // este id é opcional, pois não vai ser informado na instancia
+  // decorator @primary column o type orm faz o mapeamento do atributo id com a chave primaria da tabela
+  @PrimaryColumn()
   id?: string
+
+  @Column()
   name: string
+
+  // create date column é um decorator do typeorm especifico para data de criação
+  @CreateDateColumn()
   created_at: Date
 
   // dessa forma não vai ser criado os valores name e created_at quando instanciar a classe pelo metodo construtor
@@ -15,3 +27,5 @@ export class Role {
     }
   }
 }
+
+
--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,6 +1,7 @@
 //instancia do datasource com a conexao do banco de dados
 import { DataSource } from "typeorm"
 import { CreateRolesTable1698455439958 } from "./migrations/1698455439958-CreateRolesTable"
+import { Role } from "@roles/entities/Role"
 
 // vou chamar essa função no arquivo principal server.ts para se conectar no banco quando a aplicação subir
 const dataSource = new DataSource({
@@ -8,7 +9,7 @@ const dataSource = new DataSource({
     type: "sqlite",
     database: "./db.sqlite", //vai criar o arquivo na raiz do projeto
     // informando as entidades que vou trabalhar
-    entities: [],
+    entities: [Role],
     migrations: [ // nas migrations tenho as classes das migrations criadas
       CreateRolesTable1698455439958
     ]
