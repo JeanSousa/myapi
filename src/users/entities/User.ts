759cb05f7bff6b1e70bb5b1d1c435e3db9ddc688
jeanjr123 <jeanjr.silvasousa@gmail.com>
2023-11-25 15:00:53 -0300

    create relation users roles

--- src/users/entities/User.ts
+++ src/users/entities/User.ts
@@ -1,4 +1,5 @@
-import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm'
+import { Role } from '@roles/entities/Role'
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryColumn } from 'typeorm'
 import { v4 as uuidv4 } from 'uuid'
 
 // fazendo o mapeamento da entidade com decorators para a tabela do banco de dados
@@ -25,6 +26,16 @@ export class User {
 
   @Column()
   avatar?: boolean
+ // => Role é a entidaque qual esta se relacionando
+  @ManyToOne(() => Role, {
+    cascade: true, // quando crio um usuario posso definir a role do usuario que ele salvara junto
+    // como nesse exemplo
+    // const photo2 = new Photo()
+    // photo2.url = "me-and-bears.jpg"
+    // photo2.user = user
+    // await dataSource.manager.save(photo2)
+  })
+  role: Role // o nome role o typeorm ja entende que a chave estrangeira será roleId, e é do tipo role entity
 
   // create date column é um decorator do typeorm especifico para data de criação
   @CreateDateColumn()
